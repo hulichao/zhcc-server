07c0d735be3678b5b99fa6676d4051222aa8671a
hulichao <hulichao@meituan.com>
2018-08-24 11:16:04 +0800

    Test

--- zhcc-common/src/test/java/com/hu/Test.java
+++ zhcc-common/src/test/java/com/hu/Test.java
@@ -1,10 +1,23 @@
-package com.hu;/**
- * Created by hulichao on 2018/8/15.
- */
+package com.hu;
 
 /**
  * @author hulichao
  * @date 2018/8/15
  */
 public class Test {
+    public static void main(String[] args) {
+        String s1 = "programming";
+        String s2 = new String("programming");
+        String s3 = "program";
+        String s4 = "ming";
+        String s5 = "program" + "ming";
+        String s6 = s3 + s4;
+        String s7 = "program" + s4;
+        System.out.println(s1 == s2);//false
+        System.out.println(s1 == s5);//true
+        System.out.println(s1 == s6);//false
+        System.out.println(s1 == s6.intern());//true
+        System.out.println(s2.intern() == s1.intern());//false
+        System.out.println(s1 == s7);//false
+    }
 }
