43b56e854e5b0a5237a7797fc94074a66ec7b8e4
hulichao <hulichao@meituan.com>
2018-09-11 13:53:34 +0800

    增加php

---
+++ zhcc-common/src/test/java/com/hu/Test4.java
@@ -0,0 +1,12 @@
+package com.hu;
+
+/**
+ * @author hulichao
+ * @date 2018/9/5
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        String str = "";
+        System.out.println(str.replace(" ",""));
+    }
+}
